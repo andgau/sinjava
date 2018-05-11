3600216f02996166436ab8e2e94e2cd7d664952c
andres <andres@andres-Lenovo-B50-70>
2018-05-11 16:11:20 +0200

    Eliminado proyecto obsoleto

---
+++ probando/src/main/java/es/sinjava/App.java
@@ -0,0 +1,11 @@
+package es.sinjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo Linux Mint 18 Cinamon");
+	}
+}
