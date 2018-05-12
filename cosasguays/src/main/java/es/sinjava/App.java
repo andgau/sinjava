a72748e88a86eed4d9e813e6efc42e0f0114d8f4
andres <andres@andres-Lenovo-B50-70>
2018-05-12 16:53:39 +0200

    Proyecto cosas guays

---
+++ cosasguays/src/main/java/es/sinjava/App.java
@@ -0,0 +1,13 @@
+package es.sinjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- probando/src/main/java/es/sinjava/App.java
+++ probando/src/main/java/es/sinjava/App.java
@@ -1,11 +1,7 @@
 package es.sinjava;
 
-/**
- * Hello world!
- *
- */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String...args) {
 		System.out.println("Hola Mundo Linux Mint 18 Cinamon");
 	}
 }
